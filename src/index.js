e7a7491d2ba065a3c49d7fef3f90b70af7a16268
Alex Gadaix <alex.gadaix@gmail.com>
2021-04-02 13:51:50 +0100

    Fix the relative path to the App component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App></App>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
