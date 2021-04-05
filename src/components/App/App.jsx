1b4038c59edf5ec1ff48cbe5ed645d00e53bbc81
Alex Gadaix <alex.gadaix@gmail.com>
2021-04-05 21:57:59 +0100

    refactor app and handles local state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import * as ROUTES from '../../constants/routes';
@@ -9,21 +10,30 @@ import Navigation from '../Navigation';
 import PasswordForgetPage from '../PasswordForget';
 import SignInPage from '../SignIn';
 import SignUpPage from '../SignUp';
-const App = () => (
-  <Router>
-    <div>
-      <Navigation />
-      <hr />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { authUser: null };
+  }
+  render() {
+    const { authUser } = this.state;
+    return (
+      <Router>
+        <div>
+          <Navigation authUser={authUser} />
+          <hr />
 
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-      <Route path={ROUTES.HOME} component={HomePage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
-    </div>
-  </Router>
-);
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+          <Route path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
