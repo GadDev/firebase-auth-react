9ed0b44a4c1f4782b57cf30fad232480c91b76d7
Alex Gadaix <alex.gadaix@gmail.com>
2021-04-06 16:05:40 +0100

    adding password forget link

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,10 +1,11 @@
+import PasswordForgetLink from '../PasswordForget/PasswordForgetLink';
 import SignInForm from './SignInForm';
 import SignUpLink from './SignUpLink';
-
 const SignIn = () => (
   <div>
     <h1>SignIn</h1>
     <SignInForm />
+    <PasswordForgetLink />
     <SignUpLink />
   </div>
 );
