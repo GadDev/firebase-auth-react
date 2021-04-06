b3f873bed53e1d7d57f5c8e025650a13a6ffaa95
Alex Gadaix <alex.gadaix@gmail.com>
2021-04-06 16:39:29 +0100

    Password change and account routes

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,7 +1,16 @@
-const Account = () => (
+import React from 'react';
+
+import PasswordChangeForm from '../PasswordChange';
+import PasswordForgetForm from '../PasswordForget/PasswordForgetForm';
+
+const AccountPage = () => (
   <div>
-    <h1>Account</h1>
+    <h1>Account Page</h1>
+    <h2>Password Forget</h2>
+    <PasswordForgetForm />
+    <h2>Password change</h2>
+    <PasswordChangeForm />
   </div>
 );
 
-export default Account;
+export default AccountPage;
